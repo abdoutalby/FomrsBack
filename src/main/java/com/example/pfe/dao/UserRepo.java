82f9cc2e6c999bb5771ad31df73a6a8c97c65b82
abdou <abderrahmentalby@gmail.com>
2022-05-25 17:48:28 +0100

    users and auth

---
+++ src/main/java/com/example/pfe/dao/UserRepo.java
@@ -0,0 +1,17 @@
+package com.example.pfe.dao;
+
+import com.example.pfe.Models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+ 
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+	User getById(Integer id);
+	Optional <List<User>> findByUsernameContaining(String username);
+}
